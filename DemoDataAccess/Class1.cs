dd8a5184e6805377327f208a4e6f7d17424d8f7b
Mike Meyer <mikemey01@gmail.com>
2016-12-03 15:55:55 -0700

    initial commit

---
+++ DemoDataAccess/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoDataAccess
+{
+    public class Class1
+    {
+    }
+}

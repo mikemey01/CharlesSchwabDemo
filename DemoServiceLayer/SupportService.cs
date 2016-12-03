e7ee088acf0bd0312b3539c13a34c8b24251781f
Mike Meyer <mikemey01@gmail.com>
2016-12-03 16:31:05 -0700

    adding service layer work

---
+++ DemoServiceLayer/SupportService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using DemoDataAccess;
+using DemoServiceLayer.Models;
+
+namespace DemoServiceLayer
+{
+    public class SupportService
+    {
+        public SupportService()
+        {
+
+        }
+
+        public FundCollectionDTO GetFundCollections()
+        {
+            FundCollectionDTO dto = new FundCollectionDTO();
+
+            return dto;
+        }
+    }
+}

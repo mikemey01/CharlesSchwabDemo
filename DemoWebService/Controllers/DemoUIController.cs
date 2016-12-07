0703b02ee03fb21abd8d2863b6716b6c023e8dd2
Mike Meyer <mikemey01@gmail.com>
2016-12-07 14:13:38 -0700

    adding kendo and demoui

---
+++ DemoWebService/Controllers/DemoUIController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoWebService.Controllers
+{
+    public class DemoUIController : Controller
+    {
+        // GET: DemoUI
+        public ActionResult Index()
+        {
+            ViewResult view = View();
+            view.ViewName = "DemoUI";
+
+            return view;
+        }
+    }
+}
